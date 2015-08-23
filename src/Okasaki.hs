050d6cfe815831edd40bcb25d8eeb15f8a14d7b0
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-08-23 13:31:54 +0900

    Initial commit. Project setup and 2-1.

---
+++ src/Okasaki.hs
@@ -0,0 +1,14 @@
+module Okasaki
+     ( suffixes
+     ) where
+
+-- | 2-1. Return suffixes of a list in descending order.
+--
+-- >>> suffixes [1,2,3,4]
+-- [[1,2,3,4],[2,3,4],[3,4],[4]]
+--
+-- >>> suffixes []
+-- []
+suffixes :: [a] -> [[a]]
+suffixes [] = []
+suffixes xss@(_:xs) = xss : suffixes xs
