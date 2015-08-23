1f00a05a151ae703237ee0ee3c86cac70d3fa305
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2015-08-24 08:51:28 +0900

    Solve Exercise 2.2 and 2.3.

--- src/Okasaki/BinaryTree.hs
+++ src/Okasaki/BinaryTree.hs
@@ -21,6 +21,9 @@ class Set s a where
 -- >>> insert 5 (T (T E 3 E) 8 E)
 -- T (T E 3 (T E 5 E)) 8 E
 --
+-- >>> insert 3 (T (T E 3 E) 8 E)
+-- T (T E 3 E) 8 E
+--
 -- >>> member 3 (T (T E 3 E) 8 E)
 -- True
 --
@@ -35,12 +38,29 @@ instance Ord a => Set UnbalancedSet a where
   -- | Return an empty set.
   empty = E
 
-  -- | Insert an element to a set.
-  insert x E = T E x E
-  insert x s@(T a y b)
-    | x < y     = T (insert x a) y b
-    | x > y     = T a y (insert x b)
-    | otherwise = s
+  -- | Excersize 2.3
+  -- Insert an element to a set.
+  --
+  -- Simple implementation:
+  -- > insert x E = T E x E
+  -- > insert x s@(T a y b)
+  -- >   | x < y     = T (insert x a) y b
+  -- >   | x > y     = T a y (insert x b)
+  -- >   | otherwise = s
+  insert x s = case insert' x s of
+    Nothing -> s
+    Just ss -> ss
+    -- TODO: Abuse of Maybe?
+    -- insert' :: a -> s a -> Maybe (s a)
+    where insert' x E = Just (T E x E)
+          insert' x s@(T a y b)
+            | x < y     = case insert' x a of
+                Nothing -> Nothing
+                Just as -> Just (T as y b)
+            | x > y     = case insert' x b of
+                Nothing -> Nothing
+                Just bs -> Just (T a y bs)
+            | otherwise = Nothing
 
   -- | Excersize 2.2
   -- Return whether an element is a member of a set.
@@ -53,7 +73,8 @@ instance Ord a => Set UnbalancedSet a where
   -- >   | otherwise = True
   member x E = False
   member x (T a y b) = member' x y (T a y b)
-    where member' x c E = x == c
-          member' x c (T a y b)
-            | x < y     = member' x c a
+    -- member' :: a -> a -> s a -> Bool
+    where member' x candidate E = x == candidate
+          member' x candidate (T a y b)
+            | x < y     = member' x candidate a
             | otherwise = member' x y b
